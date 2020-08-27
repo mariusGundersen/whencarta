095a99a19a0818232a6042c5b2787e2de0b6356e
Marius Gundersen <me@mariusgundersen.net>
2020-08-27 22:26:21 +0200

    timescale and all geological times

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PanZoom from './PanZoom';
-import { modelToView } from './lib/panzoom';
+import { modelToView, Transform } from './lib/panzoom';
+import { timeline } from 'console';
 
 interface Timespan {
   start: number,
@@ -11,99 +12,165 @@ interface Timespan {
   children?: Timespan[]
 }
 
-const root = {
+interface MomentWithWidth {
+  start: number,
+  w: number,
+  label: string,
+  children?: MomentNode[]
+}
+interface MomentWithEnd {
+  start: number,
+  end: number,
+  label: string,
+  children?: MomentNode[]
+}
+
+type MomentNode = MomentWithEnd | MomentWithWidth;
+
+const root: MomentNode = {
   start: -13_000_000_000, end: 2020, label: 'The universe exists',
   children: [
-    { start: -4_600_000_000, w: 600_000_000, y: 1, label: 'Hadean eon', children: [] },
+    { start: -4_600_000_000, end: -4_000_000_000, label: 'Hadean eon' },
     {
-      start: -4_000_000_000, w: 1_500_000_000, y: 1, label: 'Archean eon', children: [
-        { start: -4_000_000_000, w: 400_000_000, y: 2, label: 'Eoarchean era' },
-        { start: -3_600_000_000, w: 400_000_000, y: 2, label: 'Paleoarchean era' },
-        { start: -3_200_000_000, w: 400_000_000, y: 2, label: 'Mesoarchean era' },
-        { start: -2_800_000_000, w: 300_000_000, y: 2, label: 'Neoarchean era' },
+      start: -4_000_000_000, w: 1_500_000_000, label: 'Archean eon', children: [
+        { start: -4_000_000_000, w: 400_000_000, label: 'Eoarchean era' },
+        { start: -3_600_000_000, w: 400_000_000, label: 'Paleoarchean era' },
+        { start: -3_200_000_000, w: 400_000_000, label: 'Mesoarchean era' },
+        { start: -2_800_000_000, w: 300_000_000, label: 'Neoarchean era' },
       ]
     },
     {
-      start: -2_500_000_000, w: 1_959_000_000, y: 1, label: 'Proterozoic eon', children: [
-        { start: -2_500_000_000, w: 900_000_000, y: 2, label: 'Paleoproterozoic era' },
-        { start: -1_600_000_000, w: 600_000_000, y: 2, label: 'Mesoproterozoic era' },
-        { start: -1_000_000_000, w: 459_000_000, y: 2, label: 'Neoproterozoic era' },
+      start: -2_500_000_000, w: 1_959_000_000, label: 'Proterozoic eon', children: [
+        {
+          start: -2_500_000_000, w: 900_000_000, label: 'Paleoproterozoic era', children: [
+            { start: -2_500_000_000, end: -2_300_000_000, label: 'Sideran' },
+            { start: -2_300_000_000, end: -2_050_000_000, label: 'Rhyacian' },
+            { start: -2_050_000_000, end: -1_800_000_000, label: 'Orosirian' },
+            { start: -1_800_000_000, end: -1_600_000_000, label: 'Statherian' },
+          ]
+        },
+        {
+          start: -1_600_000_000, w: 600_000_000, label: 'Mesoproterozoic era', children: [
+            { start: -1_600_000_000, end: -1_400_000_000, label: 'Calymmian' },
+            { start: -1_400_000_000, end: -1_200_000_000, label: 'Ectasian' },
+            { start: -1_200_000_000, end: -1_000_000_000, label: 'Stenian' },
+          ]
+        },
+        {
+          start: -1_000_000_000, w: 459_000_000, label: 'Neoproterozoic era', children: [
+            { start: -1_000_000_000, end: -720_000_000, label: 'Tonian' },
+            { start: -720_000_000, end: -635_000_000, label: 'Cryogenian' },
+            { start: -635_000_000, end: -541_000_000, label: 'Ediacaran' },
+          ]
+        },
       ]
     },
     {
-      start: -541_000_000, w: 541_000_000, y: 1, label: 'Phanerozoic eon', children: [
-        { start: -541_000_000, w: 289_098_000, y: 2, label: 'Paleozoic era' },
-        { start: -251_902_000, w: 185_902_000, y: 2, label: 'Mesozoic era' },
-        { start: -66_000_000, w: 66_000_000, y: 2, label: 'Cenozoic era' },
+      start: -541_000_000, w: 541_000_000, label: 'Phanerozoic eon', children: [
+        {
+          start: -541_000_000, w: 289_098_000, label: 'Paleozoic era', children: [
+            { start: -541_000_000, end: -485_400_000, label: 'Cambrian' },
+            { start: -485_400_000, end: -443_400_000, label: 'Ordovician' },
+            { start: -443_400_000, end: -419_200_000, label: 'Silurian' },
+            { start: -419_200_000, end: -358_900_000, label: 'Devonian' },
+            { start: -358_900_000, end: -298_900_000, label: 'Carboniferous' },
+            { start: -298_900_000, end: -251_902_000, label: 'Permian' },
+          ]
+        },
+        {
+          start: -251_902_000, w: 185_902_000, label: 'Mesozoic era', children: [
+            { start: -251_902_000, end: -201_300_000, label: 'Triassic' },
+            { start: -201_300_000, end: -145_000_000, label: 'Jurassic' },
+            { start: -145_000_000, end: -66_000_000, label: 'Cretaceous' },
+          ]
+        },
+        {
+          start: -66_000_000, w: 66_000_000, label: 'Cenozoic era', children: [
+            { start: -66_000_000, end: -23_030_000, label: 'Paleogene' },
+            { start: -23_030_000, end: -2_580_000, label: 'Neogene' },
+            {
+              start: -2_580_000, end: 2020, label: 'Quaternary', children: [
+                { start: -2_580_000, end: -11_700, label: 'Pleistocene' },
+                {
+                  start: -11_700, end: 2020, label: 'Holocene-1', children: [
+                    {
+                      start: -11_700, end: 2020, label: 'Holocene', children: [
 
+                      ]
+                    }
+                  ]
+                },
+              ]
+            },
+          ]
+        },
       ]
     },
   ]
 };
 
+interface Moment {
+  start: number,
+  y: number,
+  end: number,
+  label: string
+}
+
+function flattenChildren({ children, start, label, ...m }: MomentNode, y: number): Moment[] {
+  return [
+    {
+      start,
+      label,
+      end: 'end' in m ? m.end : start + m.w,
+      y
+    },
+    ...children?.flatMap(m => flattenChildren(m, y + 1)) ?? []
+  ];
+}
+
+const events = flattenChildren(root, 0);
+
+const timescales = [
+  { y: 0, t: 10_000_000_000, label: '10 billion' },
+  { y: 1, t: 1_000_000_000, label: '1 billion' },
+  { y: 2, t: 100_000_000, label: '100 million' },
+  { y: 3, t: 10_000_000, label: '10 million' },
+  { y: 4, t: 1_000_000, label: '1 million' },
+  { y: 5, t: 100_000, label: '100 thousand' },
+  { y: 6, t: 10_000, label: '10 thousand' },
+  { y: 7, t: 1_000, label: 'millennium' },
+  { y: 8, t: 100, label: 'century' },
+  { y: 9, t: 10, label: 'decade' },
+  { y: 10, t: 1, label: 'year' },
+];
+
+
+function limit({ x, s }: Transform) {
+  s = clamp(1 / 13000000, s, 1000);
+  x = clamp(1000 - 2020 * s, x, 13000000000 * s);
+  return ({
+    x,
+    y: 0,
+    s
+  });
+};
 
 function App() {
-  const events = [
-    { t: -13_000_000_000, w: 13_000_002_020, y: 0, label: 'The universe' },
-    { t: -4_600_000_000, w: 600_000_000, y: 1, label: 'Hadean eon' },
-
-    { t: -4_000_000_000, w: 1_500_000_000, y: 1, label: 'Archean eon' },
-
-    { t: -4_000_000_000, w: 400_000_000, y: 2, label: 'Eoarchean era' },
-    { t: -3_600_000_000, w: 400_000_000, y: 2, label: 'Paleoarchean era' },
-    { t: -3_200_000_000, w: 400_000_000, y: 2, label: 'Mesoarchean era' },
-    { t: -2_800_000_000, w: 300_000_000, y: 2, label: 'Neoarchean era' },
-
-    { t: -2_500_000_000, w: 1_959_000_000, y: 1, label: 'Proterozoic eon' },
-
-    { t: -2_500_000_000, w: 900_000_000, y: 2, label: 'Paleoproterozoic era' },
-    { t: -1_600_000_000, w: 600_000_000, y: 2, label: 'Mesoproterozoic era' },
-    { t: -1_000_000_000, w: 459_000_000, y: 2, label: 'Neoproterozoic era' },
-
-    { t: -541_000_000, w: 541_000_000, y: 1, label: 'Phanerozoic eon' },
-
-    { t: -541_000_000, w: 289_098_000, y: 2, label: 'Paleozoic era' },
-    { t: -251_902_000, w: 185_902_000, y: 2, label: 'Mesozoic era' },
-    { t: -66_000_000, w: 66_000_000, y: 2, label: 'Cenozoic era' },
-
-    { t: 2020, y: 3, label: 'Now' },
-    { t: 0, w: 2020, y: 4, label: 'The common era' },
-    { t: -10_000, y: 3, label: 'The start of the holocene' },
-
-    { t: -10_000_000_000, w: 10_000_000_000, y: 0, label: '10 billion' },
-    { t: -1_000_000_000, w: 1_000_000_000, y: 1, label: '1 billion' },
-    { t: -100_000_000, w: 100_000_000, y: 2, label: '100 million' },
-    { t: -10_000_000, w: 10_000_000, y: 3, label: '10 million' },
-    { t: -1_000_000, w: 1_000_000, y: 4, label: '1 million' },
-    { t: -100_000, w: 100_000, y: 5, label: '100 thousand' },
-    { t: -10_000, w: 10_000, y: 6, label: '10 thousand' },
-    { t: -1_000, w: 1_000, y: 7, label: 'millenium' },
-    { t: -100, w: 100, y: 8, label: 'century' },
-    { t: -10, w: 10, y: 9, label: 'decade' },
-    { t: -1, w: 1, y: 10, label: 'year' },
-    { t: 0, w: 2_000, y: 7, label: 'The common ear' },
-
-  ];
-
   return (
     <div className="App">
-      <PanZoom initialTransform={{ x: 1000, y: 0, s: 1 / 13_000_000 }} limit={({ x, y, s }) => ({ x: clamp(1000 - 2020 * s, x, 13_000_000_000 * s), y: 0, s: clamp(1 / 13_000_000, s, 1_000) })}>
+      <PanZoom initialTransform={{ x: 1000, y: 0, s: 1 / 13_000_000 }} limit={limit}>
         {(transform) => {
 
           const unitHeight = 100;
 
-          const resize = ({ x: left, y: top, width, height }: { x: number, y: number, width: number, height: number }) => ({
-            x: transform.s * left + transform.x,
-            y: transform.s * top + transform.y,
-            width: transform.s * width,
-            height: transform.s * height,
-          });
-
           const yOffset = Math.max(0, Math.log10(transform.s) + 7);
 
-          console.log(transform.x, yOffset);
           const timeToX = (t: number) => transform.s * t + transform.x;
           const transformY = (y: number) => y - yOffset * unitHeight;
+          const xToTime = (x: number) => (x - transform.x) / transform.s;
+
+          const timeLeft = xToTime(0);
+          const timeRight = xToTime(1000);
 
           const resizeTimeline = ({ x, y, width, height }: { x: number, y: number, width: number, height: number }) => ({
             x: timeToX(x),
@@ -114,12 +181,24 @@ function App() {
 
           return (
             <svg viewBox="0 0 1000 300" width="1000" height="300" style={{ border: '1px solid black' }}>
+              {timescales
+                .filter(({ y }) => y > yOffset - 1 && y < yOffset + 2)
+                .flatMap(({ y, t, label }) => {
+
+                  const markers: ReactNode[] = [];
+                  for (let i = Math.floor(timeLeft / t) * t, key = 0; i < timeRight; i += t, key++) {
+                    markers.push(<rect key={label + key} x={timeToX(i)} y={transformY(y * unitHeight)} width={timeToX(t)} height={100} fill="white" stroke="red" />)
+                  }
+
+                  return markers;
+                })}
               {events
                 .filter(({ y }) => y > yOffset - 1 && y < yOffset + 3)
-                .map(({ t, w = 0, y, label }, index) => (
-                  <React.Fragment key={index}>
-                    <rect {...resizeTimeline({ width: w, height: unitHeight, x: t, y: y * unitHeight })} fill="white" stroke="black" />
-                    <text x={timeToX(t)} y={transformY(y * unitHeight + unitHeight / 2)} dominantBaseline="middle">{label}</text>
+                .filter(({ start, end }) => start < timeRight && end > timeLeft)
+                .map(({ start, end, y, label }) => (
+                  <React.Fragment key={label}>
+                    <rect {...resizeTimeline({ width: end - start, height: unitHeight / 2, x: start, y: y * unitHeight + unitHeight / 4 })} fill="white" stroke="black" />
+                    <text x={timeToX(start)} y={transformY(y * unitHeight + unitHeight / 2)} dominantBaseline="middle">{label}</text>
                   </React.Fragment>
                 ))}
             </svg>
